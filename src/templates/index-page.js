3d8735814b98aa0f82a0afee4c3bd1c71d6156ef
agaraujo25 <agaraujo@grupoasesores.com.mx>
2021-06-07 00:14:36 -0500

    Feature Slider: Video Slider

--- src/templates/index-page.js
+++ src/templates/index-page.js
@@ -1,14 +1,16 @@
 import React from 'react'
 import Hero from '../components/Hero'
+import Carousel from '../components/Carousel'
+import Cass from '../components/Cass'
 import LastNews from '../components/LastNews'
 import Layout from '../components/Layout'
-
 
 const IndexPage = ({ data }) => {
   return (
     <Layout>
       <Hero />
       <LastNews />
+      <Carousel />
     </Layout>
   )
 }
