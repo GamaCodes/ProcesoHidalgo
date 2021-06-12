cf98f994b255a55be29e4c2b0b5edc7b555643e8
agaraujo25 <agaraujo@grupoasesores.com.mx>
2021-06-12 01:39:56 -0500

    google adsense

--- src/templates/index-page.js
+++ src/templates/index-page.js
@@ -4,15 +4,21 @@ import Carousel from '../components/Carousel'
 import Contact from '../components/Contact'
 import LastNews from '../components/LastNews'
 import Layout from '../components/Layout'
+import { Helmet } from 'react-helmet'
 
 const IndexPage = ({ data }) => {
   return (
-    <Layout>
-      <Hero />
-      <LastNews />
-      <Carousel />
-      <Contact />
-    </Layout>
+    <>
+      <Helmet titleTemplate="Prensa Hidalgo">
+        <script data-ad-client="ca-pub-1035048148988410" async src="https://pagead2.googlesyndication.com/pagead/js/adsbygoogle.js"></script>
+      </Helmet>
+      <Layout>
+        <Hero />
+        <LastNews />
+        <Carousel />
+        <Contact />
+      </Layout>
+    </>
   )
 }
 
